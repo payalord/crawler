56917bcb80002236118e5045b97e011172e0bd50
payalord <payalord@gmail.com>
2020-12-22 11:19:33 -0500

    Filters, Promises fixed

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const port = 3000
 Array.prototype.FilterSameOrigin = function(origin) {
     return this.filter((item) => {
         const href = item.getAttribute('href')
-        if (href.startsWith('/') || href.startsWith(`http://${origin}`) || href.startsWith(`https://${origin}`)) {
+        if (href.startsWith('/') || href.startsWith('#') || href.startsWith(`http://${origin}`) || href.startsWith(`https://${origin}`)) {
             return true
         }
         return false
@@ -18,42 +18,60 @@ Array.prototype.FilterSameOrigin = function(origin) {
 Array.prototype.FilterExternalOrigin = function(origin) {
     return this.filter((item) => {
         const href = item.getAttribute('href')
-        if (href.startsWith('/') || href.startsWith(`http://${origin}`) || href.startsWith(`https://${origin}`)) {
+        if (href.startsWith('/') || href.startsWith('#') || href.startsWith(`http://${origin}`) || href.startsWith(`https://${origin}`)) {
             return false
         }
         return true
     })
 }
 
-const Action = async (origin, url, processedLinks) => {
-    const data = await Crawler(url && url.startsWith('http') ? url : `https://${origin}/${url}` || origin)
+String.prototype.rmQueryString = function() {
+    return this.split(/[?#]/)[0]
+}
+
+const Action = async (origin, url, processedLinks, externalProcessedLinks) => {
+    const data = await Crawler(url && url.startsWith('http') ? url : `https://${origin}/${url}/` || origin)
     if (data) {
         const root = parse(data);
         const elements = root.querySelectorAll('a')
         
         const externalLinks = [...new Set(elements?.FilterExternalOrigin(origin))]
-        processedLinks.push(...externalLinks)
+        externalLinks.map(i => {
+            const href = i.getAttribute('href').rmQueryString()
+            if (externalProcessedLinks.findIndex(l => l.getAttribute('href').rmQueryString() === href || l.getAttribute('href').rmQueryString()+'/' === href) === -1) {
+                externalProcessedLinks.push(i)
+            }
+        })
 
         const links = [...new Set(elements?.FilterSameOrigin(origin))]
 
-        links.map(async (i) => {
-            const href = i.getAttribute('href').split(/[?#]/)[0]
-
-            if (processedLinks.findIndex(l => l.getAttribute('href') === href) === -1) {
-                processedLinks.push(i)
-                await Action(origin, href, processedLinks)
-            }
-        })
+        await Promise.all(
+            links.map(async i => {
+                const href = i.getAttribute('href').rmQueryString()
+                
+                if (processedLinks.findIndex(l => l.getAttribute('href').rmQueryString() === href || l.getAttribute('href').rmQueryString()+'/' === href) === -1) {
+                    processedLinks.push(i)
+                    await Action(origin, href, processedLinks, externalProcessedLinks)
+                }
+            })
+        )
     }
 }
 
-app.get('/', async (req, res) => {
+app.get('/', async (req, res, next) => {
     if (req.query.origin) {
         const processedLinks = []
-        await Action(req.query.origin, null, processedLinks)
+        const externalProcessedLinks = []
+        console.log('Starting to crawl, please wait...')
+        await Action(req.query.origin, null, processedLinks, externalProcessedLinks)
+        processedLinks.push(...externalProcessedLinks)
+        console.log('Full crawl completed')
         res.json(processedLinks.map(i => i.getAttribute('href')))
+        next()
+    } else {
+        res.json([])
+        next()
     }
-    res.json([])
 })
 
 app.listen(port, () => {
