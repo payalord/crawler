5452ee4e47093e091099819e3b2210a07cc1e830
payalord <payalord@gmail.com>
2020-12-22 09:53:55 -0500

    Actions, filters, recursive WIP

--- src/index.js
+++ src/index.js
@@ -5,21 +5,55 @@ import { parse } from 'node-html-parser'
 const app = express()
 const port = 3000
 
-app.get('/', async (req, res) => {
-    if (req.query.url) {
-        const data = await Crawler(req.query.url)
-        if (data) {
-            const root = parse(data);
-    
-            const links = root.querySelectorAll('a')
-    
-            res.send(links.map((item, index) => {
-                const href = item.getAttribute('href')
-                return href
-            }))
+Array.prototype.FilterSameOrigin = function(origin) {
+    return this.filter((item) => {
+        const href = item.getAttribute('href')
+        if (href.startsWith('/') || href.startsWith(`http://${origin}`) || href.startsWith(`https://${origin}`)) {
+            return true
         }
+        return false
+    })
+}
+
+Array.prototype.FilterExternalOrigin = function(origin) {
+    return this.filter((item) => {
+        const href = item.getAttribute('href')
+        if (href.startsWith('/') || href.startsWith(`http://${origin}`) || href.startsWith(`https://${origin}`)) {
+            return false
+        }
+        return true
+    })
+}
+
+const Action = async (origin, url, processedLinks) => {
+    const data = await Crawler(url && url.startsWith('http') ? url : `https://${origin}/${url}` || origin)
+    if (data) {
+        const root = parse(data);
+        const elements = root.querySelectorAll('a')
+        
+        const externalLinks = [...new Set(elements?.FilterExternalOrigin(origin))]
+        processedLinks.push(...externalLinks)
+
+        const links = [...new Set(elements?.FilterSameOrigin(origin))]
+
+        links.map(async (i) => {
+            const href = i.getAttribute('href').split(/[?#]/)[0]
+
+            if (processedLinks.findIndex(l => l.getAttribute('href') === href) === -1) {
+                processedLinks.push(i)
+                await Action(origin, href, processedLinks)
+            }
+        })
     }
-    res.send('Nothing to crawl!')
+}
+
+app.get('/', async (req, res) => {
+    if (req.query.origin) {
+        const processedLinks = []
+        await Action(req.query.origin, null, processedLinks)
+        res.json(processedLinks.map(i => i.getAttribute('href')))
+    }
+    res.json([])
 })
 
 app.listen(port, () => {
